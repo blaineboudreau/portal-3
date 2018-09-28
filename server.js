e0a87da1c1273f6965c4a338118ee305882ad82d
blaineboudreau <blaine.boudreau@gmail.com>
2018-09-27 23:44:07 -0500

    .

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 // index.js
+
+// Dependencies
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var ejs = require('ejs');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var lodash = require('lodash');
 
 var output = lodash.without([1, 2, 3], 1);
